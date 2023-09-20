ee6cfe4fb0d71b60c6359608f1b38c674702e573
天空之城 <57618347+tkzc11@users.noreply.github.com>
2023-09-20 20:50:04 +0800

    Update and rename Flags.js to Flag.js

---
+++ Flag.js
@@ -0,0 +1,5 @@
+{"name":"Flag", "description":"Flags by tkzc11", "icons":[{"name":"Ascension_Island.png","url":"https://raw.githubusercontent.com/tkzc11/Icon/main/Flags/Ascension_Island.png"}, 
+{"name":"Andorra.png","url":"https://raw.githubusercontent.com/tkzc11/Icon/main/Flags/Andorra.png"}, 
+
+{"name":"Zimbabwe.png","url":"https://raw.githubusercontent.com/tkzc11/Icon/main/Flags/Zimbabwe.png"}]}
+
